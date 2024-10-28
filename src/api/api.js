1129cb14fe4fb03c9a46701ac1241d0d2bcd2672
itsdreamy <syifasaepulbachri@gmail.com>
2024-10-28 16:44:43 +0700

    pulling new project

--- src/api/api.js
+++ src/api/api.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://73b897de8dcad809b9580d851639d2c4.serveo.net/api/v1";
-export const BASE_URL = "https://73b897de8dcad809b9580d851639d2c4.serveo.net";
+export const API_URL = "https://6b8eb9c91d185e1fab3ca25ee9173a91.serveo.net/api/v1";
+export const BASE_URL = "https://6b8eb9c91d185e1fab3ca25ee9173a91.serveo.net";
